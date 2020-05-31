bff4e2e2893b093e09525b8fda520f6dfc0c95d2
abolfazl <a.ahankoob@gmail.com>
2020-05-31 21:11:16 +0430

    Begining

---
+++ app/src/main/java/com/ahankoob/foodassistant/Activities/Main.java
@@ -0,0 +1,16 @@
+package com.ahankoob.foodassistant.Activities;
+
+import android.os.Bundle;
+
+import com.ahankoob.foodassistant.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
